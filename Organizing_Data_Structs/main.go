cfacb9deabb72f3a0d21a25f1ad6e05a079bc612
Benex Drake <benex@hotmail.de>
2023-11-04 20:47:04 +0100

    next step in course is Maps

--- Organizing_Data_Structs/main.go
+++ Organizing_Data_Structs/main.go
@@ -2,19 +2,49 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode string
+}
+
 type person struct {
 	firstname string
 	lastname  string
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{firstname: "Alex", lastname: "Anderson"}
+	alex := person{
+		firstname: "Alex",
+		lastname:  "Anderson",
+		contact: contactInfo{
+			email:   "alex@gmail.de",
+			zipCode: "12345",
+		},
+	}
 
 	var benex person
 
 	benex.firstname = "Benex"
 	benex.lastname = "Drake"
+	benex.contact.email = "sagichnicht@nope.de"
+	benex.contact.zipCode = "40000"
 
-	fmt.Println(alex)
-	fmt.Println(benex)
+	benex.print()
+	fmt.Println()
+	alex.print()
+
+	alex.updateName("Wilhelm")
+
+	fmt.Println()
+	alex.print()
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstname = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
