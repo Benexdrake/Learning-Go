83f4578a4a9ed5a3193f8a7261e260b64359a0f7
Benex Drake <benex@hotmail.de>
2023-11-05 14:49:15 +0100

    read a file

--- printfile/main.go
+++ printfile/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+
+	c, err := os.ReadFile(os.Args[1])
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	str := string(c)
+
+	fmt.Println(str)
 
 }
